72cdec2af9bc6156425b614a2f3789c275262208
diwangs <27280516+diwangs@users.noreply.github.com>
2018-09-20 00:31:42 +0700

    Fixed and trimmed hill-climb.py

--- hill-climb.py
+++ hill-climb.py
@@ -2,61 +2,25 @@ import chess
 
 b = chess.Board(8)
 b.random_fill(8)
+print("Initial (ff, kill):", b.count_total_targets())
 
 score_old = -1
 score = 0
 
 while score > score_old:
     score_old = score
-    for piece in b.pieces:
-        max_piece = None
-        max_x = None
-        max_y = None
-        try:
-            piece.move(piece.x + 1, piece.y)
-            ff, kill = b.count_total_targets()
-            if kill - ff > score:
-                score = kill - ff
-                max_piece = piece
-                max_x = piece.x
-                max_y = piece.y
-            piece.move(piece.x - 1, piece.y)            
-        except:
-            pass
-        try:
-            piece.move(piece.x - 1, piece.y)
-            ff, kill = b.count_total_targets()
-            if kill - ff > score:
-                score = kill - ff
-                max_piece = piece
-                max_x = piece.x
-                max_y = piece.y
-            piece.move(piece.x + 1, piece.y)            
-        except:
-            pass
-        try:
-            piece.move(piece.x, piece.y + 1)
-            ff, kill = b.count_total_targets()
-            if kill - ff > score:
-                score = kill - ff
-                max_piece = piece
-                max_x = piece.x
-                max_y = piece.y
-            piece.move(piece.x, piece.y - 1)            
-        except:
-            pass
-        try:
-            piece.move(piece.x, piece.y - 1)
-            ff, kill = b.count_total_targets()
-            if kill - ff > score:
-                score = kill - ff
-                max_piece = piece
-                max_x = piece.x
-                max_y = piece.y
-            piece.move(piece.x, piece.y + 1)            
-        except:
-            pass
-        if max_piece:
-            max_piece.move(max_x, max_y)
+    max_piece, max_x, max_y = None, None, None
+    for piece in b.pieces: # for every piece in the board
+        for delta_x in range(-1, 2): # Check the adjacent place
+            for delta_y in range(-1, 2):
+                try: 
+                    piece.move(piece.x + delta_x, piece.y + delta_y) # Move temporarily
+                    ff, kill = b.count_total_targets() # Check if it's valid and whether it will give best result
+                    if kill - ff > score: # if yes, store the movement
+                        score, max_piece, max_x, max_y = kill - ff, piece, piece.x, piece.y 
+                    piece.move(piece.x - delta_x, piece.y - delta_y) # Restore the previous placement
+                except ValueError: pass # If movement not valid, pass
+    if max_piece: max_piece.move(max_x, max_y) # Update the board to the best possible state, if a better state found
 
-print(b.count_total_targets())
+# print(b)
+print("Hill-climbed (ff, kill):", b.count_total_targets())
