21276b21ec2ab0c0390ee4a8aecb1f1e356f9caa
diwangs <27280516+diwangs@users.noreply.github.com>
2018-09-20 00:34:07 +0700

    Updated comments

--- hill-climb.py
+++ hill-climb.py
@@ -7,7 +7,7 @@ print("Initial (ff, kill):", b.count_total_targets())
 score_old = -1
 score = 0
 
-while score > score_old:
+while score > score_old: # While there exist an improvement that can be made...
     score_old = score
     max_piece, max_x, max_y = None, None, None
     for piece in b.pieces: # for every piece in the board
@@ -19,7 +19,7 @@ while score > score_old:
                     if kill - ff > score: # if yes, store the movement
                         score, max_piece, max_x, max_y = kill - ff, piece, piece.x, piece.y 
                     piece.move(piece.x - delta_x, piece.y - delta_y) # Restore the previous placement
-                except ValueError: pass # If movement not valid, pass
+                except ValueError: pass # if movement not valid, pass
     if max_piece: max_piece.move(max_x, max_y) # Update the board to the best possible state, if a better state found
 
 # print(b)
