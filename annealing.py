1b76d3c8ddbdee440046a09f3ae154a0aca60eeb
Restu Wahyu Kartiko <restukartiko03@gmail.com>
2018-09-27 10:27:37 +0700

    add annealing

--- annealing.py
+++ annealing.py
@@ -15,7 +15,7 @@ ff, kill = board.count_total_targets()
 score_old = ff - kill - 1
 score = kill - ff
 
-while score > score_old: # While there exist an improvement that can be made...
+for i in range(1,100): # While there exist an improvement that can be made...
     score_old = score
     max_piece, max_x, max_y = None, None, None
     for piece in board.pieces: # for every piece in the board
@@ -24,9 +24,10 @@ while score > score_old: # While there exist an improvement that can be made...
                 try: 
                     piece.move(piece.x + delta_x, piece.y + delta_y) # Move temporarily
                     ff, kill = board.count_total_targets() # Check if it's valid and whether it will give best result
-                    dE = kill - ff - score
-                    # probability = decimal.Decimal(decimal.Decimal(math.e) ** (decimal.Decimal(-dE) * decimal.Decimal(temperature)))
-                    if dE > 0 or random.uniform(0, 10) < 0.1: # if yes, store the movement
+                    delta_e = kill - ff - score
+                    # probability = decimal.Decimal(decimal.Decimal(math.e) ** (decimal.Decimal(-delta_e) * decimal.Decimal(temperature)))
+                    
+                    if delta_e > 0 or random.uniform(0, 10) < 0.1: # if yes, store the movement
                         score, max_piece, max_x, max_y = kill - ff, piece, piece.x, piece.y 
                     
                     piece.move(piece.x - delta_x, piece.y - delta_y) # Restore the previous placement
